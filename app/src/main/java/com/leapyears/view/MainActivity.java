ae10c971bdf97c81c457a0fc41bc00b1f5e47e4e
2019-SOW-DEV-003 <2019SOWDEV003@gmail.com>
2019-05-07 22:09:27 +0200

    Moved activity to view package

--- app/src/main/java/com/leapyears/view/MainActivity.java
+++ app/src/main/java/com/leapyears/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.leapyears;
+package com.leapyears.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.leapyears.R;
 
 public class MainActivity extends AppCompatActivity {
 
