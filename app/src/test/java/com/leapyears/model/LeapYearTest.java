070d5b040cc0ecd9e56db1457ed3a5fb2bab1577
2019-SOW-DEV-003 <2019SOWDEV003@gmail.com>
2019-05-07 21:05:12 +0200

    Refactored Leap year test, moved object creation to setup method

--- app/src/test/java/com/leapyears/model/LeapYearTest.java
+++ app/src/test/java/com/leapyears/model/LeapYearTest.java
@@ -1,5 +1,6 @@
 package com.leapyears.model;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -10,20 +11,25 @@ import static junit.framework.Assert.assertTrue;
 @RunWith(MockitoJUnitRunner.class)
 public class LeapYearTest {
 
+    private static final int NORMAL_YEAR = 2017;
+    private static final int LEAP_YEAR = 2012;
+    private LeapYear model;
+
+    @Before
+    public void setUp() {
+        model = new LeapYear();
+    }
+
     @Test
     public void shouldReturnFalse_ForNormalYearYear() {
-        LeapYear model =  new LeapYear();
-
-        boolean result = model.isLeapYear(2017);
+        boolean result = model.isLeapYear(NORMAL_YEAR);
 
         assertFalse(result);
     }
 
     @Test
     public void shouldReturnTrue_ForLeapYear() {
-        LeapYear model =  new LeapYear();
-
-        boolean result = model.isLeapYear(2012);
+        boolean result = model.isLeapYear(LEAP_YEAR);
 
         assertTrue(result);
     }
