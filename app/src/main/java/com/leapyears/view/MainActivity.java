d6183031a065e4521712c5e715e31b59a7637c98
2019-SOW-DEV-003 <2019SOWDEV003@gmail.com>
2019-05-07 22:17:01 +0200

    Added Observer and OnclickListner

--- app/src/main/java/com/leapyears/view/MainActivity.java
+++ app/src/main/java/com/leapyears/view/MainActivity.java
@@ -1,18 +1,22 @@
 package com.leapyears.view;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.leapyears.R;
+import com.leapyears.model.LeapYear;
 import com.leapyears.viewmodel.LeapYearViewModel;
 import com.leapyears.viewmodelfactory.ViewModelFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private LeapYearViewModel viewModel;
     private EditText edtYear;
@@ -32,8 +36,36 @@ public class MainActivity extends AppCompatActivity {
         txtResult = findViewById(R.id.txt_result);
         container = findViewById(R.id.container);
 
-        ViewModelFactory factory = new ViewModelFactory(this.getApplication(), new LeapYearUtils());
+        ViewModelFactory factory = new ViewModelFactory(this.getApplication(), new LeapYear());
         viewModel = ViewModelProviders.of(this, factory).get(LeapYearViewModel.class);
 
+        btnFind.setOnClickListener(this);
+        btnClear.setOnClickListener(this);
+        container.setOnClickListener(this);
+
+        addObserver();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_clear){
+            edtYear.setText("");
+            edtYear.setError(null);
+        } else if(v.getId() == R.id.button_find){
+            if(edtYear.getText().toString().equals("")){
+                edtYear.setError("Please enter a value");
+            } else{
+                viewModel.find(Integer.valueOf(edtYear.getText().toString()));
+            }
+        }
+    }
+
+    private void addObserver() {
+        viewModel.getResultLiveData().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String result) {
+                txtResult.setText(result);
+            }
+        });
     }
 }
