968f00381da8eb3116490f3d83eea840c9dce9bb
2019-SOW-DEV-003 <2019SOWDEV003@gmail.com>
2019-05-07 22:39:27 +0200

    Added Espresso test for checking app context

---
+++ app/src/androidTest/java/com/leapyears/view/MainActivityTest.java
@@ -0,0 +1,27 @@
+package com.leapyears.view;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.leapyears", appContext.getPackageName());
+    }
+}
