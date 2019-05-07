f5099a126afc83c0eabaa4efed3d17ea6bd3b0f7
2019-SOW-DEV-003 <2019SOWDEV003@gmail.com>
2019-05-07 21:30:36 +0200

    Refactored isLeapYear method

--- app/src/test/java/com/leapyears/model/LeapYear.java
+++ app/src/test/java/com/leapyears/model/LeapYear.java
@@ -3,14 +3,30 @@ package com.leapyears.model;
 class LeapYear {
 
     boolean isLeapYear(int year) {
-        if(year < 1582){
+        if(isJulianYear(year)){
             return false;
-        } else if(isMultipleOf(year, 400)){
+        } else if(isSpecialLeapYear(year)){
             return true;
-        } else if(isMultipleOf(year, 100) && !(isMultipleOf(year, 400))){
+        } else if(isSpecialNormalYear(year)){
             return false;
         } else
-            return isMultipleOf(year, 4) && !(isMultipleOf(year, 100));
+            return isLeap(year);
+    }
+
+    private boolean isLeap(int year) {
+        return isMultipleOf(year, 4) && !(isMultipleOf(year, 100));
+    }
+
+    private boolean isSpecialNormalYear(int year) {
+        return isMultipleOf(year, 100) && !(isMultipleOf(year, 400));
+    }
+
+    private boolean isJulianYear(int year) {
+        return year < 1582;
+    }
+
+    private boolean isSpecialLeapYear(int year) {
+        return isMultipleOf(year, 400);
     }
 
     private boolean isMultipleOf(int year, int i) {
