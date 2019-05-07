b07e1c6d575bc43542c65e2dcc7808ebbcced06b
2019-SOW-DEV-003 <2019SOWDEV003@gmail.com>
2019-05-07 22:14:15 +0200

    Linked Viewmodel to view(Activity)

--- app/src/main/java/com/leapyears/view/MainActivity.java
+++ app/src/main/java/com/leapyears/view/MainActivity.java
@@ -1,15 +1,39 @@
 package com.leapyears.view;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.leapyears.R;
+import com.leapyears.viewmodel.LeapYearViewModel;
+import com.leapyears.viewmodelfactory.ViewModelFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LeapYearViewModel viewModel;
+    private EditText edtYear;
+    private TextView txtResult;
+    private Button btnFind;
+    private Button btnClear;
+    private ConstraintLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFind = findViewById(R.id.button_find);
+        btnClear = findViewById(R.id.button_clear);
+        edtYear = findViewById(R.id.edt_leapyear);
+        txtResult = findViewById(R.id.txt_result);
+        container = findViewById(R.id.container);
+
+        ViewModelFactory factory = new ViewModelFactory(this.getApplication(), new LeapYearUtils());
+        viewModel = ViewModelProviders.of(this, factory).get(LeapYearViewModel.class);
+
     }
 }
